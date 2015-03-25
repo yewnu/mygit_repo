cfd3ee3246cbde3c612d0e06d63f4af904efcb4f
yewnu <yirgalem.dejen@gmail.com>
2015-03-25 09:17:43 -0400

    commit 4

--- src/myproject/MyGitTester.java
+++ src/myproject/MyGitTester.java
@@ -4,6 +4,7 @@ public class MyGitTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("This is yirgalem, Hello Every Body, this my first git");
 		System.out.println("Hello Every Body, this my first git, added from git");
 		SecondClass.hello();
 	}
