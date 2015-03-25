ff282f76186e729a7addb2ce6a41be46c78fcc3b
yewnu <yirgalem.dejen@gmail.com>
2015-03-25 09:08:51 -0400

    test commit

---
+++ src/myproject/MyGitTester.java
@@ -0,0 +1,10 @@
+package myproject;
+
+public class MyGitTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Every Body");
+	}
+
+}
