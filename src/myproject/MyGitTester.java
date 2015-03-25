2cf35ac4324450016be2f647086bbb8af2b1df4d
yewnu <yirgalem.dejen@gmail.com>
2015-03-25 09:15:13 -0400

    third commit

--- src/myproject/MyGitTester.java
+++ src/myproject/MyGitTester.java
@@ -4,7 +4,7 @@ public class MyGitTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Every Body");
+		System.out.println("Hello Every Body, this my first git");
 		SecondClass.hello();
 	}
 
